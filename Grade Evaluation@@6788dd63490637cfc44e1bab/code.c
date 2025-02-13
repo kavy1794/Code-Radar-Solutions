fc47999807d1e459c03c1629720b0020fc55c0b4
kavy1794 <kavy1794.becse24@chitkara.edu.in>
2025-02-13 17:53:08 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a=='A'){
+    printf("Excellent");
+    }
+    else if(a==2){
+        printf("28");
+    }
+     else if(a==3){
+        printf("31");
+    }
+     else if(a==4){
+        printf("30");
+    }
+     else if(a==12){
+        printf("31");
+    }
+    else
+    printf("Invalid month");
+    return 0;
+}
