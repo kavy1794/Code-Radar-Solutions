60bb430df6fcf7e6ac9282eee0de9af6070e8452
kavy1794 <kavy1794.becse24@chitkara.edu.in>
2025-02-18 09:08:08 +0530

    Solution to Check Triangle Type

--- Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
+++ Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
@@ -3,12 +3,6 @@ int main()
 {
     int a,b,c;
     scanf("%d %d %d", &a, &b, &c);
-
-    // if(a <= 0 || b <= 0 || c <= 0) {
-    //     printf("The sides of a triangle must be positive integers.\n");
-    // } else if(a + b <= c || a + c <= b || b + c <= a) {
-    //     printf("The given lengths do not form a triangle.\n");
-    // } else {
         if(a == b && b == c) {
             printf("Equilateral\n");
         } else if(a == b || a == c || b == c) {
@@ -16,7 +10,5 @@ int main()
         } else {
             printf("Scalene\n");
         }
-    // }
-
     return 0;
 }
