4b191ab68bed99760299ad64a607ff1064cb3692
kavy1794 <kavy1794.becse24@chitkara.edu.in>
2025-03-16 11:33:39 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -7,37 +7,33 @@ struct Student {
     float marks;
 };
 
+void sortStudents(struct Student students[], int n) {
+    struct Student temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (students[i].marks < students[j].marks) {
+                temp = students[i];
+                students[i] = students[j];
+                students[j] = temp;
+            }
+        }
+    }
+}
+
 int main() {
     int n;
     scanf("%d", &n);
 
     struct Student students[n];
-    struct Student minStudent;
-
     for (int i = 0; i < n; i++) {
         scanf("%d %s %f", &students[i].rollNumber, students[i].name, &students[i].marks);
-        
-        if (i == 0 || students[i].marks < minStudent.marks) {
-            minStudent = students[i];
-        }
     }
 
-    for (int i = 0; i < n; i++) {
-        if (students[i].rollNumber == 2) {
-            printf("Roll Number: %d, Name: %s, Marks: %.2f\n", students[i].rollNumber, students[i].name, students[i].marks);
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        if (students[i].rollNumber == 1) {
-            printf("Roll Number: %d, Name: %s, Marks: %.2f\n", students[i].rollNumber, students[i].name, students[i].marks);
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        if (students[i].rollNumber == 3) {
-            printf("Roll Number: %d, Name: %s, Marks: %.2f\n", students[i].rollNumber, students[i].name, students[i].marks);
-        }
-    }
+    sortStudents(students, n);
 
+    for (int i = 0; i < n; i++) {
+        printf("Roll Number: %d, Name: %s, Marks: %.2f\n", students[i].rollNumber, students[i].name, students[i].marks);
+    }
 
     return 0;
 }
