730e6951cc4c3b9d2be40a12a1544f138aa13294
kavy1794 <kavy1794.becse24@chitkara.edu.in>
2025-03-16 11:40:37 +0530

    Solution to Movie Ticket Booking System

---
+++ Movie Ticket Booking System@@67877d61dc107486ad5c2751/code.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+struct MovieTicket {
+    char movieName[50];
+    char ticketType[20];
+    float ticketPrice;
+};
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    struct MovieTicket tickets[N];
+    float totalStandard = 0, totalPremium = 0, totalVIP = 0;
+
+    for (int i = 0; i < N; i++) {
+        scanf("%s %s %f", tickets[i].movieName, tickets[i].ticketType, &tickets[i].ticketPrice);
+
+        if (strcmp(tickets[i].ticketType, "Standard") == 0) {
+            totalStandard += tickets[i].ticketPrice;
+        } else if (strcmp(tickets[i].ticketType, "Premium") == 0) {
+            totalPremium += tickets[i].ticketPrice;
+        } else if (strcmp(tickets[i].ticketType, "VIP") == 0) {
+            totalVIP += tickets[i].ticketPrice;
+        }
+    }
+
+    printf("Standard: %.2f\n", totalStandard);
+    printf("Premium: %.2f\n", totalPremium);
+    printf("VIP: %.2f\n", totalVIP);
+
+    return 0;
+}
