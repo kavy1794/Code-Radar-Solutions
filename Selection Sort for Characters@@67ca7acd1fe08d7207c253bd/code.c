5a4345c6e77b6692add195b2e7f48744cce6bbf2
kavy1794 <kavy1794.becse24@chitkara.edu.in>
2025-03-19 18:00:12 +0530

    Solution to Selection Sort for Characters

--- Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
+++ Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
@@ -1,35 +1,20 @@
-#include <stdio.h>
-void selectionSort(char arr[], int n) {
-    int i, j, min_idx;
-    char temp;
-    for (i = 0; i < n-1; i++) {
-        min_idx = i;
-        for (j = i+1; j < n; j++) {
-            if (arr[j] < arr[min_idx]) {
-                min_idx = j;
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
             }
         }
-        temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
+       
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 }
-
-void printArray(char arr[], int n) {
+void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++) {
-        printf("%c ", arr[i]);
+        printf("%d ", arr[i]);
     }
     printf("\n");
-}
-
-int main() {
-    int n;
-    scanf("%d", &n);
-    char arr[n];
-    for (int i = 0; i < n; i++) {
-        scanf(" %c", &arr[i]);
-    }
-    selectionSort(arr, n);
-    printArray(arr, n);
-    return 0;
 }
