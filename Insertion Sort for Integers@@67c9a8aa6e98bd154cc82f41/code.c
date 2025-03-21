6a650cb1a619b6fbceb89b9d9240d55e92e082fa
kavy1794 <kavy1794.becse24@chitkara.edu.in>
2025-03-21 16:20:14 +0530

    Solution to Insertion Sort for Integers

--- Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
+++ Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
@@ -1,25 +1,18 @@
 
-#include<stdio.h>
-#include<string.h>
-void insertionSort(char arr[][100], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
-            if (strcmp(arr[j], arr[minIndex]) < 0) {
-                minIndex = j;
+#include <stdio.h>
+int insertionSort(int arr[], int n){
+    for (int i = 0; i<n-1; i++){
+        for (int j = 0; j<n-i-1; j++){
+            if (arr[j]>arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
             }
-        }
-
-        if (minIndex != i) {
-            char temp[100];
-            strcpy(temp, arr[i]);
-            strcpy(arr[i], arr[minIndex]);
-            strcpy(arr[minIndex], temp);
         }
     }
 }
-void printArray(char arr[][100], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%s\n", arr[i]);
+int printArray(int arr[], int n){
+    for (int i = 0; i<n;i++){
+        printf("%d ",arr[i]);
     }
 }
